d1d2b021ce8fb565b44a00be04f1eb6374bfd77d
root <root@ip-172-31-21-104.eu-central-1.compute.internal>
2018-06-21 01:56:01 +0000

    second commit

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 app.set('view engine', 'hbs');
 
 hbs.registerPartials(__dirname + '/views/Partials');
-app.use((req,res,next)=> {
+/*app.use((req,res,next)=> {
 res.render('maintenance.hbs');
 });
 app.use(express.static(__dirname + '/public'));
-
+*/
 app.use((req,res,next)=> {
 var now = new Date().toString();
 var log = `${now}: ${req.method} ${req.url}`;
